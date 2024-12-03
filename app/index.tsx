8c02c2311aab868cfbcba092a82051fd6dde1d39
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-03 12:17:18 +0545

    redirect to onboarding page added

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,6 @@
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
+
 const index = () => {
-  return (
-    <SafeAreaView>
-      <Text>index</Text>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/(auth)/welcome"} />;
 };
 export default index;

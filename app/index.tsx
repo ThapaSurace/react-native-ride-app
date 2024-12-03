43dc8ec32e9808f0ec07ea8fd08b11dbf68cc633
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-03 11:53:43 +0545

    added fonts and images

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Text>index</Text>
+    </SafeAreaView>
+  );
+};
+export default index;

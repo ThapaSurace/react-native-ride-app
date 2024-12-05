c78b0c6eb35209ef97b51ab07460b6fe2b1e2b95
Suresh Thapa <zorosenpai456@gmail.com>
2024-12-05 07:56:29 +0545

    displayed signined user info

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from "react-native";
-const Home = () => {
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomePage() {
+  const { user } = useUser();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut>
+    </SafeAreaView>
   );
-};
-export default Home;
+}
